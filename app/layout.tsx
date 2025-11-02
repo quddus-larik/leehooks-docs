30ac82de8a3b20fe615a55b4a711bc0c338c1a41
quddus-larik <quddus@proton.me>
2025-11-02 13:58:51 +0500

    update assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import { Providers } from "@/providers"
 import { GoogleTagManager } from "@next/third-parties/google"
 
